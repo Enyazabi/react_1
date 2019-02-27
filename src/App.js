afe8128804a0c79dfb8f3a936c740f942afe523a
enya <essidanaya@gmail.com>
2019-02-27 23:22:24 +0300

    Create LoginComponent

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+
 import './App.css';
 
+import SignIn from "./Components/LoginComponent";
+import ButtonAppBar from "./Components/NavBar";
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div >
+            <ButtonAppBar/>
+          <SignIn/>
       </div>
     );
   }
