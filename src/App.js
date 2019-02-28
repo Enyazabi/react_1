af2ac6933b1aac2a00bc98d1fd38bd5562a87d73
enya <essidanaya@gmail.com>
2019-02-28 22:40:40 +0300

    Create menu component

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
 import React from 'react';
 import {BrowserRouter as Router, Route} from 'react-router-dom'
-
 import {createBrowserHistory} from 'history'
-
-
 import SignIn from "./Components/LoginComponent";
 import ButtonAppBar from "./Components/NavBar";
+import SimpleMenu from "./Components/MenuComponent";
+
+
+
 const App =() =>(
-
         <Router history={createBrowserHistory}>
-
       <div>
             <ButtonAppBar/>
             <Route path="/login" component={SignIn}/>
-
       </div>
+
         </Router>
-
-
-
 )
 
 export default App
