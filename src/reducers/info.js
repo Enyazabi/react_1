188f0016f1928a98962421091d3fa8cf54dbdd78
enya <essidanaya@gmail.com>
2019-03-04 23:55:37 +0300

    add some changes to reducers

--- src/reducers/info.js
+++ src/reducers/info.js
@@ -2,7 +2,11 @@ const initialState = {
     user: 'unknown user'
 }
 
-export default function userInfo (state = initialState) {
-    return state
-    
+export default function userInfo (state = initialState, action) {
+    switch (action.type) {
+        case 'USER_FIRST_ACTION':
+            return {...state, user: action.payload}
+        default:
+            return state
+    }
 }
