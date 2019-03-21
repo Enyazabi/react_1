5982a90c8ac287ed06fac3c827cc80faad8ddc87
enya <essidanaya@gmail.com>
2019-03-22 00:52:27 +0300

    ass NewsContainer

--- src/containers/App.js
+++ src/containers/App.js
@@ -3,16 +3,16 @@ import {BrowserRouter as Router, Route} from 'react-router-dom'
 import {createBrowserHistory} from 'history'
 import SignIn from "../Components/LoginComponent";
 import ButtonAppBar from "../Components/NavBar";
-import SimpleCard from "../Components/NewsComponent";
 import SimpleList from "../Components/ProfilePage";
 import { connect } from "react-redux";
 import userAction from "../Actions/userAction";
+import NewsContainer from "./NewsContainer";
 
 const App =() =>(
         <Router history={createBrowserHistory}>
       <div>
             <ButtonAppBar/>
-            <Route path="/news" component={SimpleCard}/>
+            <Route path="/news" component={NewsContainer}/>
             <Route path="/login" component={SignIn}/>
             <Route path="/profile" component={SimpleList}/>
 
